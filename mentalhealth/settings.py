31feb06ee55f9e1273fecb77f8aa34dcd8fadfdd
Sparsh Codes <singhsparsh@kccemsr.edu.in>
2021-04-05 21:10:45 +0530

    New

--- mentalhealth/settings.py
+++ mentalhealth/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
